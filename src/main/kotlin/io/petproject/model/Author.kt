02feb1ff52de64ad9896d7f3794444ab4e26bb13
Bruno <bruno.ariev@gmail.com>
2019-09-29 02:16:35 -0300

    implementing validation for Author

--- src/main/kotlin/io/petproject/model/Author.kt
+++ src/main/kotlin/io/petproject/model/Author.kt
@@ -1,4 +1,11 @@
 package io.petproject.model
 
 data class Author(val username: String,
-                  val name: String)
+                  val name: String) {
+
+    init {
+        require(username.isNotBlank()) { "Username must not be blank" }
+        require(name.isNotBlank()) { "Author name must not be blank" }
+    }
+
+}
