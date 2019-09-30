57ffa1bd812256579beaec87b87b5d4d7a957c6a
Bruno <bruno.ariev@gmail.com>
2019-09-30 00:56:26 -0300

    implementing publish and retrieve on Service

--- src/main/kotlin/io/petproject/service/ArticleService.kt
+++ src/main/kotlin/io/petproject/service/ArticleService.kt
@@ -1,22 +1,35 @@
 package io.petproject.service
 
 import io.petproject.model.Article
+import io.petproject.model.ArticleMetadata
+import io.petproject.model.Author
 import io.petproject.repository.ArticleRepository
+import io.petproject.repository.AuthorRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import javax.transaction.Transactional
 
 @Service
 @Transactional
-class ArticleService @Autowired constructor(val articleRepo: ArticleRepository) {
+class ArticleService @Autowired constructor(val articleRepo: ArticleRepository,
+                                            val authorRepo: AuthorRepository) {
 
-    fun publish(article: Article) {
-        TODO("persist article in DB")
+    fun publish(article: Article): Article {
+        val authors: List<Author> = article.meta.authors.map {
+            authorRepo.findByUsername(it.username) ?:
+            authorRepo.saveAndFlush(it)
+        }
+        val document = Article(
+            headline = article.headline, content = article.content, summary = article.summary,
+            meta = ArticleMetadata(article.meta.publishDate, article.meta.tags, authors)
+        )
+        return articleRepo.save(document)
     }
 
     fun retrieve(id: Long): Article? {
-        TODO("retrieve article from DB")
+        return articleRepo.findByIdOrNull(id)
     }
 
     fun archive(id: Long): Boolean {
