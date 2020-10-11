32a79c5365ede8a82f9227662cecbd3797e5da51
iobruno <bruno.ariev@gmail.com>
2020-10-11 08:51:47 -0300

    extracting authors from metadata

--- src/main/kotlin/io/petproject/model/ArticleMetadata.kt
+++ src/main/kotlin/io/petproject/model/ArticleMetadata.kt
@@ -1,31 +1,23 @@
 package io.petproject.model
 
-import com.fasterxml.jackson.annotation.JsonManagedReference
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 import javax.persistence.*
-import javax.persistence.CascadeType.*
 import javax.persistence.FetchType.LAZY
 
 @Embeddable
 data class ArticleMetadata(
-        @Column(name = "publishDate")
-        @JsonProperty("publishDate") val publishDate: LocalDate,
+    @Column(name = "publishDate")
+    @JsonProperty("publishDate")
+    val publishDate: LocalDate,
 
-        @ElementCollection(fetch = LAZY)
-        @CollectionTable(name = "article_tags")
-        @JsonProperty("tags") val tags: MutableList<String>,
-
-        @ManyToMany(cascade = [PERSIST, DETACH, MERGE, REFRESH])
-        @JoinTable(name = "authors_articles",
-                joinColumns = [JoinColumn(name = "article_id")],
-                inverseJoinColumns = [JoinColumn(name = "author_id")])
-        @JsonManagedReference
-        @JsonProperty("authors") val authors: MutableList<Author>) {
+    @CollectionTable(name = "article_tags")
+    @ElementCollection(fetch = LAZY)
+    @JsonProperty("tags")
+    val tags: MutableList<String>
+) {
 
     init {
         require(tags.isNotEmpty()) { "There must be at least one tag label for the article" }
-        require(authors.isNotEmpty()) { "There must be at least one author for the article" }
     }
-
 }
--- src/test/kotlin/io/petproject/model/ArticleMetadataTest.kt
+++ src/test/kotlin/io/petproject/model/ArticleMetadataTest.kt
@@ -1,33 +1,15 @@
 package io.petproject.model
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import java.lang.IllegalArgumentException
 import java.time.LocalDate
 
 internal class ArticleMetadataTest {
 
     @Test
     fun `when tags is empty, it should throw IllegalArgEx`() {
-        assertThrows<IllegalArgumentException> {
-            ArticleMetadata(
-                    publishDate = LocalDate.now(),
-                    tags = mutableListOf(),
-                    authors = mutableListOf(
-                            Author("john.doe", "John Doe"),
-                            Author("jane.doe", "Jane Doe"))
-            )
-        }
+        assertThatThrownBy {
+            ArticleMetadata(publishDate = LocalDate.now(), tags = mutableListOf())
+        }.isInstanceOf(IllegalArgumentException::class.java)
     }
-
-    @Test
-    fun `when authors is empty, it should throw IllegalArgEx`() {
-        assertThrows<IllegalArgumentException> {
-            ArticleMetadata(
-                    publishDate = LocalDate.now(),
-                    authors = mutableListOf(),
-                    tags = mutableListOf("cassandra", "aws"))
-        }
-    }
-
 }
